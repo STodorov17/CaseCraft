7145524a7c1d6c3975a91e3aed26c322cbe320c8
STodorov17 <dunavfk2310@abv.bg>
2025-08-30 14:40:44 +0300

    initial commit

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -3,10 +3,6 @@ import { stripe } from '@/lib/stripe'
 import { headers } from 'next/headers'
 import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
-// import { Resend } from 'resend'
-// import OrderReceivedEmail from '@/components/emails/OrderReceivedEmail'
-
-// const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function POST(req: Request) {
   try {
@@ -70,25 +66,6 @@ export async function POST(req: Request) {
           },
         },
       })
-
-    //   await resend.emails.send({
-    //     from: 'CaseCraft <svetlomirtodorov17@gmail.com>',
-    //     to: [event.data.object.customer_details.email],
-    //     subject: 'Thanks for your order!',
-    //     react: OrderReceivedEmail({
-    //       orderId,
-    //       orderDate: updatedOrder.createdAt.toLocaleDateString(),
-    //       // @ts-ignore
-    //       shippingAddress: {
-    //         name: session.customer_details!.name!,
-    //         city: shippingAddress!.city!,
-    //         country: shippingAddress!.country!,
-    //         postalCode: shippingAddress!.postal_code!,
-    //         street: shippingAddress!.line1!,
-    //         state: shippingAddress!.state,
-    //       },
-    //     }),
-    //   })
     }
 
     return NextResponse.json({ result: event, ok: true })
